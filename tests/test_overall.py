cd85cecf9e5b17f3ea0882309bbe6285573fb3d8
Joey C <joeycyq@gmail.com>
2018-01-03 14:13:46 +0800

    Clear tables before and after the test class

--- tests/test_overall.py
+++ tests/test_overall.py
@@ -5,18 +5,30 @@ from unittest import mock
 from stickertaggerbot import config, models, conversations, handlers
 
 from tests import telegram_factories
-from tests.misc import app_for_testing as app
-
+from tests.misc import app_for_testing, clear_all_tables
 
 route = "/" + config.TELEGRAM_TOKEN
 
 
 def post(update):
-    with app.test_client() as app_client:
+    with app_for_testing.test_client() as app_client:
         return app_client.post(route,
-                           data=update.to_json(),
-                           content_type="application/json")
+                               data=update.to_json(),
+                               content_type="application/json")
 
+
+@pytest.fixture(scope="class", autouse=True)
+def clear_tables():
+    with app_for_testing.app_context():
+        existing_users = models.User.query.all()  #TODO: Is checking neccesary?
+        if len(existing_users) > 0:
+            clear_all_tables()
+            models.database.session.commit()
+
+    yield
+    with app_for_testing.app_context():
+        clear_all_tables()
+        models.database.session.commit()
 
 @pytest.mark.incremental
 class TestNormalUsage(object):
@@ -30,9 +42,10 @@ class TestNormalUsage(object):
     @pytest.yield_fixture(autouse=True)
     def patch_telegram(self):
         patches = []
-        patches.append(mock.patch.object(app.bot, "send_message"))
-        patches.append(mock.patch.object(app.bot, "answer_inline_query"))
-        patches.append(mock.patch.object(app.bot, "send_sticker"))
+        patches.append(mock.patch.object(app_for_testing.bot, "send_message"))
+        patches.append(
+            mock.patch.object(app_for_testing.bot, "answer_inline_query"))
+        patches.append(mock.patch.object(app_for_testing.bot, "send_sticker"))
 
         for patch in patches:
             patch.start()
@@ -41,10 +54,6 @@ class TestNormalUsage(object):
             patch.stop()
 
     def test_start(self):
-        with app.app_context():
-            existing_users = models.User.query.all()
-        assert len(existing_users) == 0
-
         update = telegram_factories.CommandUpdateFactory(
             command="/start",
             message__from_user=self.user,
@@ -53,12 +62,12 @@ class TestNormalUsage(object):
         response = post(update)
         time.sleep(2)
 
-        with app.app_context():
+        with app_for_testing.app_context():
             database_user = models.User.get(update.effective_user.id)
 
         assert database_user is not None
 
-        app.bot.send_message.assert_called_once_with(
+        app_for_testing.bot.send_message.assert_called_once_with(
             self.chat.id, handlers.Message.Instruction.START.value)
 
     def test_new_sticker(self):
@@ -72,7 +81,7 @@ class TestNormalUsage(object):
         conversation = conversations.all[self.user.id]
         assert conversation.sticker == self.sticker
 
-        app.bot.send_message.assert_called_once_with(
+        app_for_testing.bot.send_message.assert_called_once_with(
             self.chat.id, handlers.Message.Instruction.LABEL.value)
 
     def test_label(self):
@@ -86,7 +95,7 @@ class TestNormalUsage(object):
         conversation = conversations.all[self.user.id]
         assert conversation.labels == self.labels
 
-        app.bot.send_sticker.assert_called_once_with(
+        app_for_testing.bot.send_sticker.assert_called_once_with(
             self.chat.id, self.sticker)
 
     def test_confirm(self):
@@ -102,7 +111,7 @@ class TestNormalUsage(object):
         response = post(update)
         time.sleep(2)
 
-        with app.app_context():
+        with app_for_testing.app_context():
             sticker_ids = models.Association.get_sticker_ids(
                 self.user.id, self.labels)
 
@@ -110,18 +119,18 @@ class TestNormalUsage(object):
         assert len(set(sticker_ids)) == 1
         assert sticker_ids[0] == self.sticker.file_id
 
-        app.bot.send_message.assert_called_once_with(
+        app_for_testing.bot.send_message.assert_called_once_with(
             self.chat.id, handlers.Message.Other.SUCCESS.value)
 
     def test_retrieve(self):
         update = telegram_factories.InlineQueryUpdateFactory(
             inline_query__query=self.labels[0],
-            inline_query__bot=app.bot,
+            inline_query__bot=app_for_testing.bot,
             inline_query__from_user=self.user)
 
         post(update)
         time.sleep(2)
 
         result = [handlers.StickerResult(self.sticker.file_id)]
-        app.bot.answer_inline_query.assert_called_once_with(
+        app_for_testing.bot.answer_inline_query.assert_called_once_with(
             update.inline_query.id, result, is_personal=True)
